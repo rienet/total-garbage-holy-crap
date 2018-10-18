2798164b9a898d6b8e1d31966133e80b7a840806
aryaanpala <44216982+aryaanpala@users.noreply.github.com>
2018-10-18 19:44:08 +1030

    Update newclub.cpp

--- newclub.cpp
+++ newclub.cpp
@@ -1,17 +1,32 @@
 #include "newclub.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
 void NewClub::action(University uniObject){
 	textEvent = "Some new clubs have been popping up and attracting new "
 	"students to the uni. You gain double the amount of students you had "
 	"before, and is now a club master";
+	cout<<"Some new clubs have been popping up and attracting new "
+	"students to the uni. You gain double the amount of students you had "
+	"before, and is now a club master"<<endl;
 
 	int moreStudents = uniObject.getStudents();
 	moreStudents = moreStudents*2;
 	newStudents = moreStudents;	
+	newMoney=uniObject.getMoneyReserve();
+	newHate=uniObject.getHate();
 }
 
 int NewClub::getStudents() {
 	return newStudents;
+}
+
+int NewClub::getMoney() {
+	return newMoney;
+}
+int NewClub::getHate() {
+	return newHate;
 }
 
 void NewClub::displayEvent() {
